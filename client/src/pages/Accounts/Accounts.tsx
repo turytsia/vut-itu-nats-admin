cd99abd322114ad4ad8c2f5eefa108210f7d6343
pomo <mark.barzali@firma.seznam.cz>
2023-11-07 19:02:52 +0100

    fix: update Account page with new modal and detail components

--- client/src/pages/Accounts/Accounts.tsx
+++ client/src/pages/Accounts/Accounts.tsx
@@ -7,15 +7,16 @@
 import React, {useCallback, useContext, useEffect, useState} from 'react'
 import Page from '../../components/Page/Page'
 import {AppContext} from "../../context/AppContextProvider";
-import {AccountType, OperatorPayloadType} from "../../utils/axios";
+import {AccountPayloadType, AccountType} from "../../utils/axios";
 import Table, {TableConfigType} from "../../components/Table/Table";
 import {defaultFiltersConfig} from "../../utils/views/tables";
 import Button from "../../components/Button/Button";
 import {Icon} from "@iconify/react";
 import icons from "../../utils/icons";
-import CreateOperatorModal from "../Operators/modals/CreateOperatorModal/CreateOperatorModal";
 import {dateFormat} from "../../utils/common";
 import {columns, ColumnTypes} from "../../hooks/useSort";
+import CreateAccountModal from "./modals/CreateAccountModal";
+import AccountRowActions from "./components/AccountRowActions/AccountRowActions";
 
 export const accountHeaderMap = {
     "operator": "Operator",
@@ -45,23 +46,25 @@ export type ExtendedAccountType = AccountType & { operator: string }
 const Accounts = () => {
     const {request} = useContext(AppContext)
     const [isLoading, setIsLoading] = useState<boolean>(false)
-    const [accounts, setAccounts] = useState<AccountType[]>([])
+    const [accounts, setAccounts] = useState<ExtendedAccountType[]>([])
 
     const [error, setError] = useState<string>("")
 
     const [isCreateActive, setIsCreateActive] = useState<boolean>(false)
 
+    const [operators, setOperators] = useState<string[]>([])
     const fetchAccounts = useCallback(
         async () => {
             setIsLoading(true);
 
             try {
-                // fetch all operators
-                const {operators} = await request.get.operators();
+                // fetch all operators,
+                //  operators will be set via hook but in lazy mode, due to fetch data races.
+                const operatorsResponse = (await request.get.operators()).operators
 
                 const fullFilledResponseSet: ExtendedAccountType[] = [];
                 // fetch all users
-                operators.forEach((operator: string): void => {
+                operatorsResponse.forEach((operator: string): void => {
                     (async () => {
                         const accountListResponsesSet = await Promise.allSettled(
                             (await request.get.accounts(operator)).accounts.map(
@@ -79,11 +82,12 @@ const Accounts = () => {
                             let m = t as ExtendedAccountType;
                             m.operator = operator;
                             return m
-                        } ))
+                        }))
                     })()
                 })
 
                 console.log(fullFilledResponseSet)
+                setOperators(operatorsResponse)
                 setAccounts(fullFilledResponseSet)
             } catch (error) {
                 console.error(error)
@@ -98,7 +102,7 @@ const Accounts = () => {
      * Content function for the cell values in the table.
      */
     const renderContent = useCallback(
-        (key: string, account: any) => {
+        (key: string, account: ExtendedAccountType) => {
             switch (key) {
                 case "operator":
                 case "name":
@@ -108,30 +112,34 @@ const Accounts = () => {
                 case "iat":
                     return dateFormat(account[key])
                 case "":
-                    return ""
+                    return (
+                        <AccountRowActions operatorName={account.operator} account={account} />
+                    )
             }
         },
-        [accounts]
+        []
     )
 
     /**
      * Submits form to create new operator
      */
     const onAccountSubmit = useCallback(
-        async (form: OperatorPayloadType) => {
+        async (form: AccountPayloadType) => {
             try {
-                const response = await request.post.operator(form)
+                const response = await request.post.account(form.operator, form)
 
                 if (response.type === "error") {
                     setError(response.data?.message || "An error occurred.");
                 } else {
                     setError("");
+                    setIsCreateActive(false)
+                    fetchAccounts()
                 }
             } catch (e) {
                 console.error(e)
             }
         },
-        [request]
+        [request, fetchAccounts]
     )
 
     useEffect(() => {
@@ -154,10 +162,11 @@ const Accounts = () => {
                 }
             />
             {isCreateActive && (
-                <CreateOperatorModal
+                <CreateAccountModal
                     error={error}
                     onSubmit={onAccountSubmit}
                     onClose={() => setIsCreateActive(false)}
+                    operatorList={operators}
                 />
             )}
         </Page>
